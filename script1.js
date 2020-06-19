59907eeb975f883ef4bd976ac27151fbbbbdee37
jujulili1 <tyulianto0795@gmail.com>
2020-06-19 15:25:49 +0700

    first commit

---
+++ script1.js
@@ -0,0 +1,6 @@
+import { tambah, kurang, bagi, kali } from "./script2.js";
+
+console.log(tambah(4, 2));
+console.log(kurang(4, 2));
+console.log(bagi(4, 2));
+console.log(kali(4, 2));
---
+++ script2.js
@@ -0,0 +1,20 @@
+let a = 4;
+let b = 2;
+
+function tambah(a, b) {
+    return a + b;
+}
+
+function kurang(a, b) {
+    return a - b;
+}
+
+function bagi(a, b) {
+    return a / b;
+}
+
+function kali(a, b) {
+    return a * b;
+}
+
+export { tambah, kurang, bagi, kali };
